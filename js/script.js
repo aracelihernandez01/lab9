4884c2250b9451c7b785b4ffb42661f274b9c5ad
aracelihernandez01 <ahernandez60@hawk.iit.edu>
2024-04-02 21:58:33 -0500

    code fixed

--- js/script.js
+++ js/script.js
@@ -2,3 +2,7 @@ function init(){
     var button = document.getElementById('entrybutton');
   
     function showMeText(){
+      var textbox = document.getElementById('entryinput');
+      document.getElementById('textoutput').innerHTML = textbox.value;
+      alert("Araceli Hernandez: " + textbox.value);
+    }
